1c78227f60fab2362393639ab31c0d1e3c72bb05
Danielyilma <yilmad752@gmail.com>
2024-03-07 20:22:58 +0300

    configuring a server

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+#setting the web server for deployment
+
+apt install nginx -y
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+
+printf "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+" > /data/web_static/releases/test/index.html
+
+if [ -L /data/web_static/current ]
+then
+    rm -r /data/web_static/current
+fi
+ln -s /data/web_static/releases/test/ /data/web_static/current
+
+chown -R ubuntu:ubuntu /data/
+
+printf "server {
+    listen 80;
+
+    index index.html index.htm;
+    
+    location /hbnb_static {
+        alias /data/web_static/current/;
+        index index.html index.htm;
+    }
+}
+" > /etc/nginx/sites-available/default
+service nginx restart
