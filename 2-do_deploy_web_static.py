7ed014666705b023e4ce887fe6ee89fd19e01189
Danielyilma <yilmad752@gmail.com>
2024-03-09 13:10:07 +0300

    deploying web static files to a server

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
-
+"""
+The function `do_deploy` deploys a compressed archive to a
+remote server and creates symbolic links for the current deployment.
+"""
 from fabric.api import env, put, run, sudo
 import os
 from sys import argv
 
+
 env.hosts = ['54.237.124.13', '34.207.120.149']
-    
+
 
 def do_deploy(archive_path):
+    '''This Python function `do_deploy` is designed to deploy a
+    compressed archive to a remote server and create symbolic links
+    for the current deployment. Here is a breakdown of what the code is
+     doing'''
     if not os.path.exists(archive_path):
         return False
     filename = archive_path[9:archive_path.index('.')]
     try:
         put(archive_path, '/tmp/')
         run(f'mkdir -p /data/web_static/releases/{filename}')
-        run(f'tar -xzf /tmp{archive_path[8:]} -C /data/web_static/releases/{filename}')
+        run(f'tar -xzf /tmp{archive_path[8:]} -C /data/web_static/\
+releases/{filename}')
         run(f'rm /tmp{archive_path[8:]}')
-        run(f'mv /data/web_static/releases/{filename}/web_static/* /data/web_static/releases/{filename}/')
+        run(f'mv /data/web_static/releases/{filename}/web_static/*\
+ /data/web_static/releases/{filename}/')
         run(f'rm -rf /data/web_static/releases/{filename}/web_static')
         run('rm -rf /data/web_static/current')
         run(f'ln -s /data/web_static/releases/{filename}\
@@ -24,4 +34,3 @@ def do_deploy(archive_path):
     except Exception as f:
         return False
     return True
-    
