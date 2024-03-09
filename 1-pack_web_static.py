9bb98912e7d7f4ef95283b6361370fce8f43890c
Danielyilma <yilmad752@gmail.com>
2024-03-09 10:27:17 +0300

    running shell script locally

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
+"""
+The function `do_pack` creates a compressed archive of the 'web_static'
+folder with a timestamp in the filename.
+"""
 from fabric.api import *
 from datetime import datetime
 import os
 
 
 def do_pack():
+    """
+    The function `do_pack` creates a compressed archive of a specified
+    directory and saves it in a 'versions' folder.
+    """
     if not os.path.exists('versions'):
         os.mkdir('versions')
 
