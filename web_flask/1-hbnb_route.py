6c4737fddb2e699dfca0da1a91dd793b01328fe5
Danielyilma <yilmad752@gmail.com>
2024-03-22 19:01:57 +0300

    flask web application

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''flask application'''
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+@app.route('/<name>', strict_slashes=False)
+def hello(name=None):
+    '''root route handle'''
+    return f'HBNB' if name else f'hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
