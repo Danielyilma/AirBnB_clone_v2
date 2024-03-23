001097b21676c1cff1646bdfc552819f939f3b72
Danielyilma <yilmad752@gmail.com>
2024-03-23 19:01:54 +0300

    display state

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -8,11 +8,23 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route('/states', strict_slashes=False)
 def list_states():
     '''states list end point'''
     states = storage.all(State)
     return render_template('7-states_list.html', states=states)
+
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def states_id(id):
+    '''states list end point'''
+    state = None
+    for s_obj in storage.all(State).values():
+        if s_obj.id == id:
+            state = s_obj
+            break
+    return render_template('9-states.html', state=state)
 
 
 @app.teardown_appcontext
