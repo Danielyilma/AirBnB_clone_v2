194657310c0c0d817991b94b480410a47c05795c
Danielyilma <yilmad752@gmail.com>
2024-03-23 15:50:10 +0300

    display state

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+'''state list end point'''
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -9,6 +10,7 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def list_states():
+    '''states list end point'''
     states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
